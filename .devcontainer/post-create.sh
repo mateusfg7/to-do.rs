56e9ed87f23c35e0235a13c4e5f4f51e0c911446
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2024-02-15 16:37:08 +0000

    chore(devcontainer): update post-create script

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -10,9 +10,9 @@ sh -c "$(wget -O- https://github.com/deluan/zsh-in-docker/releases/download/v1.1
    -p https://github.com/zdharma-continuum/fast-syntax-highlighting
 
 # Setup GIT
-git config --global alias.co checkout
-git config --global alias.cm checkout main
-git config --global alias.cb checkout -b
-git config --global alias.st status -sb
-git config --global alias.lg log --pretty=format:'%Cred%h%Creset %C(bold)%cr%Creset %Cgreen<%an>%Creset %s' --max-count=30
-git config --global alias.rollback reset --soft HEAD~1
+git config --global alias.co "checkout"
+git config --global alias.cm "checkout main"
+git config --global alias.cb "checkout -b"
+git config --global alias.st "status -sb"
+git config --global alias.lg "log --pretty=format:'%Cred%h%Creset %C(bold)%cr%Creset %Cgreen<%an>%Creset %s' --max-count=30"
+git config --global alias.rollback "reset --soft HEAD~1"
